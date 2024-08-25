bed126bda65ce75b1582f10ee7d9c74defa83742
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 21:54:32 +0300

    Add files via upload

---
+++ main (2).php
@@ -0,0 +1,21 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// IF YAPISI
+$emre = 62;
+$uni = 60;
+switch ($emre) {
+    case 63:
+        echo("Urfaya Selamlar");
+        break;
+    
+    default:
+        echo("Trabzona selamlar");
+        break;
+}
+
+
+?>
